c4f9d0926fc0e5308c87c1f8c190ffc0bb96b4a2
Shrija <shrija.tumma1994@gmail.com>
2024-11-09 18:14:03 +0530

    View Template of Component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-header',
-    template: '<h3>EKart</h3>'
+    templateUrl: './header.component.html'
 })
 export class HeaderComponent {
 
