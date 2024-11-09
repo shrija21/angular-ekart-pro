4eaa7fd1d9597deacfd597498730f7e17cb6e000
Shrija <shrija.tumma1994@gmail.com>
2024-11-09 20:25:27 +0530

    Styling View Template - by styleUrls

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
-    styles: ['a{text-decoration: none; margin: 0px 10px}', 'button{padding: 10px 20px;}']
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
 
