a45b7541f0cb1b2609ca9d78582d43a3a680b7fa
Shrija <shrija.tumma1994@gmail.com>
2024-11-10 21:20:23 +0530

    Creating Component using Angular CLI - part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
+import { MainMenuComponent } from './header/main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    HeaderComponent, TopHeaderComponent
+    HeaderComponent, 
+    TopHeaderComponent, 
+    TopMenuComponent, 
+    MainMenuComponent
   ],
   imports: [
     BrowserModule,
