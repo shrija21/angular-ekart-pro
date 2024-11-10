bcb21ac0b7ac421b470035527074337a847f0d8b
Shrija <shrija.tumma1994@gmail.com>
2024-11-10 23:58:18 +0530

    Types of Component Selector - reverted all

--- src/app/top-header/top-header.component.ts
+++ src/app/top-header/top-header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: '#top-header',
+  selector: 'top-header',
   templateUrl: './top-header.component.html',
   styleUrl: './top-header.component.css'
 })
