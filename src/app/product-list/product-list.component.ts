c8c8a92b89dcb7f16dce1fde575ba186fe222050
Shrija <shrija.tumma1994@gmail.com>
2024-11-25 22:56:54 +0530

    Event Binding - Part 4

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,13 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
-  name = "John Doe";
+  // name = "John Doe";
+  addToCart:number = 0;
   product = {
     name: 'iPhone 13',
     price: 999,
     color: 'Matt Black',
     discount: 8.5,
-    inStock: 5,
+    inStock: 10,
     pImage: '/assets/images/iPhone13_1.jpg'
   }
 
@@ -21,6 +22,18 @@ export class ProductListComponent {
   }
 
   onNameChange(event: any) {
-    this.name = event.target.value;
+    // this.name = event.target.value;
+  }
+
+  decrementCartValue() {
+    if(this.addToCart > 0) {
+      this.addToCart--;
+    }
+  }
+
+  incrementCartValue() {
+    if(this.addToCart < this.product.inStock) {
+      this.addToCart++;
+    }
   }
 }
